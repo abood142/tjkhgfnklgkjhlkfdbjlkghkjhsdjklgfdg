943a4d3a0b0ff751e83ad0987f9bcddc788ba784
abood142 <44087788+abood142@users.noreply.github.com>
2018-10-13 18:31:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -206,29 +206,6 @@ client.on("message", message => {
                           }
 });
 
-client.on('message', message => {
-    if (message.content.includes('discord.gg')){      //شيل المسافه
-                        if(!message.channel.guild) return message.reply ('');
-                    if (!message.member.hasPermissions(['MANAGE_MESSAGES'])){
-       message.channel.send('kick <@' + message.author.id + '>');
-       message.delete() 
-       }
-    }
-          if (message.content.startsWith("kick ")) {
-             if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply();
-             var member= message.mentions.members.first();
-             member.ban().then((member) => {
-                 message.channel.sendMessage("", {embed: {
-                 author: {
-                 },
-                 title: 'بسبب النشر ' + member.displayName + ' تم طرد',
-                 color: 490101,
-                 }
-               });
-           }
-         ) 
-       }
-	   });
 	client.on('message', message => {
     if (message.content.startsWith("$hack")) {
         if(!message.author.id === '') return;
