c6223a8f69c708f31037609c5cebfd40d6a41af5
abood142 <44087788+abood142@users.noreply.github.com>
2018-10-13 18:44:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,6 +10,14 @@ client.on('message', msg => {
     msg.reply('Pong!');
   }
 });
+client.on('message', message => {
+if (message.content.split(' ')[0] == '$bc')
+ message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("CONNECT"))  return;
+member.send( `${member} ! ` + "**" + " : ** " + message.content.substr(3));
+                                                            message.delete();
+});
+});
 
 
 client.login(process.env.BOT_TOKEN);
